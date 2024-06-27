e77890afc89cbd6d4c09a07bfa955c96e120d184
serhii-iljin <serhii.iljin@gmail.com>
2024-06-27 15:38:12 +0100

    More refactoring: improved code structure and readability. Updated rom/ram to load them easier to logisim. The system risc_v_rv32im.circ consists entirely of new JAVA components.

--- src/main/java/com/cburch/logisim/riscv/StoreInstruction.java
+++ src/main/java/com/cburch/logisim/riscv/StoreInstruction.java
@@ -5,31 +5,20 @@ import com.cburch.logisim.data.Value;
 
 public class StoreInstruction {
 
-    static long newData;
-    static long address;
-
-    public static void latch(rv32imData hartData, long data) {
-        System.out.println("X" + newData + " " + data);
-        StoreInstruction.storeIntermixedData(hartData, data);
-        hartData.setOutputDataWidth(4);
-        hartData.setMemRead(Value.TRUE);
-        hartData.setMemWrite(Value.TRUE);
-        hartData.setIsSync(Value.TRUE);
-    }
-
     public static void performAddressing(rv32imData hartData) {
         hartData.setFetching(false);
         hartData.setAddressing(true);
 
-        newData = getNewData(hartData).toLongValue();
-        address = getAddress(hartData).toLongValue();
+        hartData.setLastDataIn(getNewData(hartData).toLongValue());
+        hartData.setLastAddress(getAddress(hartData).toLongValue());
 
         // Values for outputs fetching data
-        hartData.setAddress(Value.createKnown(32, address - (address % 4) ) );
+        hartData.setAddress(Value.createKnown(
+                32, hartData.getLastAddress() - get4LSB(hartData) ) );
 
         // Check for correct offset
         if (hartData.getOutputDataWidth() == 4) {
-            if (address % 4 != 1 && address % 4 != 3) {
+            if (get4LSB(hartData) != 1 && get4LSB(hartData) != 3) {
                 hartData.halt();
                 hartData.setMemWrite(Value.FALSE);
                 return;
@@ -37,7 +26,7 @@ public class StoreInstruction {
         }
 
         if (hartData.getOutputDataWidth() == 8) {
-            if (address % 4 != 2) {
+            if (get4LSB(hartData) != 2) {
                 hartData.halt();
                 hartData.setMemWrite(Value.FALSE);
                 return;
@@ -50,7 +39,41 @@ public class StoreInstruction {
 
     }
 
-    public static Value getNewData(rv32imData hartData) {
+    public static void storeIntermixedData(rv32imData hartData, long data) {
+        switch (hartData.getOutputDataWidth()) {
+            case 1:
+                if (get4LSB(hartData) == 0) {
+                    data -= (data & 0xFF);
+                    hartData.setOutputData(Value.createKnown(32, data | hartData.getLastDataIn()));
+                } else if (get4LSB(hartData) == 1) {
+                    data -= (data & 0xFF00);
+                    hartData.setOutputData(Value.createKnown(32, data | (hartData.getLastDataIn() << 8) ) );
+                } else if (get4LSB(hartData) == 2) {
+                    data -= (data & 0xFF0000);
+                    hartData.setOutputData(Value.createKnown(32, data | (hartData.getLastDataIn() << 16) ) );
+                } else if (get4LSB(hartData) == 3) {
+                    data -= (data & 0xFF000000L);
+                    hartData.setOutputData(Value.createKnown(32, data | (hartData.getLastDataIn() << 24) ) );
+                }
+                break;
+
+            case 4:
+                if (get4LSB(hartData) == 1) {
+                    data -= (data & 0xFFFF);
+                    hartData.setOutputData(Value.createKnown(32, data | hartData.getLastDataIn()));
+                } else if (get4LSB(hartData) == 3) {
+                    data -= (data & 0xFFFF0000L);
+                    hartData.setOutputData(Value.createKnown(32, data | (hartData.getLastDataIn() << 16) ) );
+                }
+                break;
+
+            case 8:
+                hartData.setOutputData(Value.createKnown(32, hartData.getLastDataIn()));
+                break;
+        }
+    }
+
+    private static Value getNewData(rv32imData hartData) {
         InstructionRegister ir = hartData.getIR();
         int rs2 = ir.rs2();
         long data = hartData.getX(rs2);
@@ -78,47 +101,17 @@ public class StoreInstruction {
         return result;
     }
 
-    public static void storeIntermixedData(rv32imData hartData, long data) {
-        switch (hartData.getOutputDataWidth()) {
-            case 1:
-                if (address % 4 == 0) {
-                    data -= (data & 0xFF);
-                    hartData.setOutputData(Value.createKnown(32, data | newData));
-                } else if (address % 4 == 1) {
-                    data -= (data & 0xFF00);
-                    hartData.setOutputData(Value.createKnown(32, data | (newData << 8) ) );
-                } else if (address % 4 == 2) {
-                    data -= (data & 0xFF0000);
-                    hartData.setOutputData(Value.createKnown(32, data | (newData << 16) ) );
-                } else if (address % 4 == 3) {
-                    data -= (data & 0xFF000000L);
-                    hartData.setOutputData(Value.createKnown(32, data | (newData << 24) ) );
-                }
-                break;
-
-            case 4:
-                if (address % 4 == 1) {
-                    data -= (data & 0xFFFF);
-                    hartData.setOutputData(Value.createKnown(32, data | newData));
-                } else if (address % 4 == 3) {
-                    data -= (data & 0xFFFF0000L);
-                    hartData.setOutputData(Value.createKnown(32, data | (newData << 16) ) );
-                }
-                break;
-
-            case 8:
-                hartData.setOutputData(Value.createKnown(32, newData));
-                break;
-        }
-    }
-
-    public static Value getAddress(rv32imData hartData) {
+    private static Value getAddress(rv32imData hartData) {
         InstructionRegister ir = hartData.getIR();
         int rs1 = ir.rs1();
         long imm_S = ir.imm_S();
         long baseAddress = hartData.getX(rs1);
         long address = baseAddress + imm_S;
         return Value.createKnown(BitWidth.create(32), address);
+    }
+
+    private static long get4LSB(rv32imData hartData) {
+        return (hartData.getLastAddress() & 0x3);
     }
 
 }
