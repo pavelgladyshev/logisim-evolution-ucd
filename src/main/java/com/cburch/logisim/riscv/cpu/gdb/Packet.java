99d6a5b96c99563a133d0453ebd54be169403e67
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2024-08-22 19:22:30 +0530

    Added method to handle requests.

--- src/main/java/com/cburch/logisim/riscv/cpu/gdb/Packet.java
+++ src/main/java/com/cburch/logisim/riscv/cpu/gdb/Packet.java
@@ -1,56 +1,72 @@
 package com.cburch.logisim.riscv.cpu.gdb;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Packet {
 
-    private final byte[] data;
-    private final int len;
     private final String packetData;
     private int checksum;
 
+    // constructor for pre-existing packets to be decoded
     public Packet(byte[] data, int len){
-        this.data = data;
-        this.len = len;
-        packetData = getPacketData();
+        packetData = decodePacket(data, len);
+    }
+
+    // constructor for new packets to be encoded
+    public Packet(String packetData) {
+        this.packetData = packetData;
+        this.checksum = computeChecksum();
+    }
+
+    public Boolean isValid(){
+        return !(isACK()) && !(isNACK()) && checksum != 0 && checksum == computeChecksum();
+    }
+
+    public Boolean isNACK() {
+        return Objects.equals(packetData, "-");
+    }
+
+    public Boolean isACK() {
+        return Objects.equals(packetData, "+");
     }
 
     public String getPacketData() {
-            StringBuilder packetData = new StringBuilder();
-            int i;
-            for (i = 0; i < len; i++) {
-                char x = (char) this.data[i];
-                if (x == '#') {
-                    break;
-                } else if (x != '$') {
-                    packetData.append(x);
-                }
-            }
-            try {
-                String checksum = "" + (char) data[i + 1] + (char) data[i + 2];
-                this.checksum = Integer.parseUnsignedInt(checksum, 16);
-            } catch( Exception ex){
-                this.checksum = 0;
-            }
-            return packetData.toString();
+        return packetData;
     }
 
-    public Boolean isACK(){
-        return checksum == 0;
+    public int getChecksum() {
+        return checksum;
     }
 
-    public Boolean isValidPacketData(){
-        return this.checksum == computeChecksum(packetData);
-    }
-
-    //return ascii
     public String getChecksumAsHexString(){
-        return Integer.toHexString(checksum);
+        return String.format("%02X", getChecksum());
     }
 
-    //return checksum as int
-    public static int computeChecksum(String s){
+    public String wrapped() {
+        return String.format("$%s#%s", getPacketData(), getChecksumAsHexString());
+    }
+    private String decodePacket(byte[] data, int len) {
+        StringBuilder packetData = new StringBuilder();
+        int i = 0;
+        while(i < len && (char) data[i] != '$') i++;
+        while(++i < len && (char) data[i] != '#') {
+            packetData.append((char) data[i]);
+        }
+        try {
+            String checksum = "" + (char) data[i + 1] + (char) data[i + 2];
+            this.checksum = Integer.parseUnsignedInt(checksum, 16);
+        } catch( Exception ex){
+            this.checksum = 0;
+            return (char) data[0] + "";
+        }
+        return packetData.toString();
+    }
+
+    private int computeChecksum(){
         int sum = 0;
-        for(int i = 0; i < s.length(); i++){
-            sum += s.charAt(i);
+        for(int i = 0; i < packetData.length(); i++){
+            sum += packetData.charAt(i);
         }
         return Math.floorMod(sum, 256) & 0xff;
     }
